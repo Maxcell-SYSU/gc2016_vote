b5e73f20496d327cc27ee72e0902bf98d00d126b
MtZero <zhonglsh7@mail2.sysu.edu.cn>
2016-11-18 16:11:19 +0800

    update

--- data.php
+++ data.php
@@ -6,4 +6,4 @@
  * Time: 上午9:49
  */
 $movies = array();
-$movies[0] = array("id" => 1, "aid" => "212109", "cid" => "344380", "name" => "Pump♂It");
+$movies[0] = array("id" => 1, "aid" => "212109", "cid" => "344380", "title" => "Pump♂It", "team" => "Bilibili");
