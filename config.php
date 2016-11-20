f8417fd2d5dd6a44e1882809c24bc98ba361f74d
Alcanderian <alcanderian@gmail.com>
2016-11-20 08:15:26 +0800

    add confs

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+*
+* Created by PhpStorm.
+* User: Jerry
+* Date: 2016/11/16
+* Time: 上午8:37
+*/
+$db_hostname = "localhost";
+$db_username = "maxcell";
+$db_password = "";
+$db_database = "maxcell";
+$db_table_list = "vote";
+$db_table_sum = "";
+define("CAPTCHA_ID", "");
+define("PRIVATE_KEY", "");
---
+++ data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jerry
+ * Date: 2016/11/16
+ * Time: 上午9:49
+ */
+$movies = array();
+$movies[0] = array("id" => 1, "aid" => "", "cid" => "",
+    "title" => "", "team" => "");
+
+
